c471bab4c1bb683cebb829afddd2715d45257015
Louis G <louis.guilbert@free.fr>
2022-09-07 16:13:35 +0200

    feat: replace ref-param in mongo query (#14)

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,7 +1,8 @@
 defmodule QueryParser.Parser do
   @moduledoc """
-    This Parser module will use the grammar to parse the query string into an AST.
+  This Parser module will use the grammar to parse the query string into an AST.
   """
+  alias LenraCommon.Errors.DevError
   alias QueryParser.Errors.BusinessError
   alias QueryParser.Parser.Grammar
 
@@ -50,6 +51,41 @@ defmodule QueryParser.Parser do
   end
 
   defp replace(e, _params) do
+    e
+  end
+
+  @param_regex ~r/^@(?!@)[a-zA-Z_$][a-zA-Z_$0-9]*(\.[a-zA-Z_$][a-zA-Z_$0-9]*)*$/
+
+  @doc """
+    This function will take a valid mongo query and replace all param-ref (@foo.bar)
+    to replace them with the corresponding value in the params map.
+
+    ex:
+    > Parser.replace_params(%{"foo" => "@me"}, %{"me" => "bar"})
+    %{"foo" => "bar"}
+  """
+  @spec replace_params(term(), map()) :: term()
+  def replace_params(map, params) when is_map(map) and is_map(params) do
+    map
+    |> Enum.map(fn {k, v} -> {k, replace_params(v, params)} end)
+    |> Map.new()
+  end
+
+  def replace_params(list, params) when is_list(list) do
+    Enum.map(list, fn e -> replace_params(e, params) end)
+  end
+
+  def replace_params(str, params) when is_bitstring(str) do
+    if String.match?(str, @param_regex) do
+      "@" <> str_path = str
+      path = String.split(str_path, ".")
+      get_in(params, path)
+    else
+      str
+    end
+  end
+
+  def replace_params(e, _params) do
     e
   end
 end
