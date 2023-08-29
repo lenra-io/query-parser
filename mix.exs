5175375817f80648cf9a017762e0f1ae6c4f3af6
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-08-29 10:53:06 +0200

    docs: mix.exs informations

--- mix.exs
+++ mix.exs
@@ -4,12 +4,15 @@ defmodule QueryParser.MixProject do
   def project do
     [
       app: :query_parser,
-      version: "0.0.0-dev",
+      version: "0.0.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
+      source_url: repo(),
       dialyzer: [
         plt_add_apps: [:ex_unit]
       ]
@@ -40,5 +43,21 @@ defmodule QueryParser.MixProject do
       {:neotomex, "~> 0.1.7"},
       {:poison, "~> 4.0", only: [:test]}
     ]
+  end
+
+  defp description() do
+    "This repository provides a tool that can parse a JSON query into an AST tree and parse this tree into an Ecto query that can be executed within elixir."
+  end
+
+  defp package() do
+    [
+      # only for private packages organization: "lenra",
+      licenses: ["AGPL-3.0"],
+      links: %{"GitHub" => repo()}
+    ]
+  end
+
+  defp repo() do
+    "https://github.com/lenra-io/query_parser"
   end
 end
